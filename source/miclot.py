29c12b7dbfb8bb397cd1bf9d08ace0fc63e6db64
Tom Miclot <86794938+TMiclot@users.noreply.github.com>
2024-03-28 18:44:17 +0100

    Create miclot.py

---
+++ source/miclot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+  ___      ___   __     ______   ___        ______  ___________  
+ |"  \    /"  | |" \   /" _  "\ |"  |      /    " \("     _   ") 
+  \   \  //   | ||  | (: ( \___)||  |     // ____  \)__/  \\__/  
+  /\\  \/.    | |:  |  \/ \     |:  |    /  /    ) :)  \\_ /     
+ |: \.        | |.  |  //  \ _   \  |___(: (____/ //   |.  |     
+ |.  \    /:  | /\  |\(:   _) \ ( \_|:  \\        /    \:  |     
+ |___|\__/|___|(__\_|_)\_______) \_______)\"_____/      \__|
+
+ Molecular InteraCtion anaLysis tOolkiTs
+ _______________________________________
+ 
+ A collectin of tools to analyse protein-protein interactions.
+"""
+
+__author__ = 'Tom MICLOT  <tom.miclot@jh-inst.cas.cz>'
+__license__ = "xxxx"
+__version__ = "Version: 1.0 -- jj/mm/2024"
+
+
+#=====[ Import all scripts ]=====
+
+
+#=====[ ]=====
+if __name__ == "__main__":
+    main()
+
