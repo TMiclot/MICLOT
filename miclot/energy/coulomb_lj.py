1177acd5990ddb61a458fbddb89a23abe3cd9058
Tom Miclot <86794938+TMiclot@users.noreply.github.com>
2024-04-04 19:06:42 +0200

    Create coulomb_lj.py

---
+++ miclot/energy/coulomb_lj.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This script is part of MICLOT ...
+"""
+
+__author__ = 'Tom MICLOT  <tom.miclot@jh-inst.cas.cz>'
+__license__ = "xxxx"
+__version__ = "Version: 1.0 -- jj/mm/2024"
+
+
+
+
+
+#=====================================================
+#===== Import modules
+#=====================================================
+
+
